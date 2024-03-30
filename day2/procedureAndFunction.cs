e87e4379e384061bf865a4c5fd14662add8b3a43
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2024-03-30 12:50:10 +0700

    make simple program implementation procedure and function

---
+++ day2/procedureAndFunction.cs
@@ -0,0 +1,57 @@
+Public Class Form1
+    Sub bersih()
+        txtKodeBarang.Clear()
+        txtNamaBarang.Clear()
+        txtSatuan.Clear()
+        txtHargaSatuan.Clear()
+        txtJumlah.Clear()
+        txtTotalHarga.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Call bersih()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call bersih()
+    End Sub
+
+    'define function hitung
+    Private Function Hitung()
+        Dim jumlah, hargaSatuan, totalHarga As Integer
+        'grab data from form
+        jumlah = txtJumlah.Text
+        hargaSatuan = txtHargaSatuan.Text
+        totalHarga = jumlah * hargaSatuan
+
+        'set total to form
+        txtTotalHarga.Text = totalHarga
+        Return totalHarga
+    End Function
+
+    'define cek data kosong
+    Sub cekDataKosong()
+        If txtKodeBarang.Text = "" Then
+            MessageBox.Show("Kode Barang Harus Diisi Terlebih Dahulu", "Konfirmasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtKodeBarang.Focus()
+        ElseIf txtNamaBarang.Text = "" Then
+            MessageBox.Show("Nama Barang Harus Diisi Terlebih Dahulu", "Konfirmasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtNamaBarang.Focus()
+        ElseIf txtSatuan.Text = "" Then
+            MessageBox.Show("Satuan Barang Harus Diisi Terlebih Dahulu", "Konfirmasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtSatuan.Focus()
+        ElseIf txtHargaSatuan.Text = "" Then
+            MessageBox.Show("Harga Satuan Barang Harus Diisi Terlebih Dahulu", "Konfirmasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtHargaSatuan.Focus()
+        ElseIf txtJumlah.Text = "" Then
+            MessageBox.Show("Jumlah Barang Harus Diisi Terlebih Dahulu", "Konfirmasi", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtJumlah.Focus()
+        Else
+            Call Hitung()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call cekDataKosong()
+    End Sub
+End Class
